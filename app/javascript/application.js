b73f1731a44eb0037be09737392856dfd42b98ed
cuerci <lucas.cuerci25@gmail.com>
2024-11-30 12:52:05 -0300

    varias coisas kkkk

---
+++ app/javascript/application.js
@@ -0,0 +1,12 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import "bootstrap"
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/service-worker.js")
+      .then(() => console.log("Service Worker registrado com sucesso."))
+      .catch(error => console.error("Erro ao registrar Service Worker:", error));
+}
+
+console.log("Hello from application.js");
